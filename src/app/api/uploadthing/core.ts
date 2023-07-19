784977e456f1bd9b6d86074a774a71b2e1f04622
Mingz <n_minghao28@hotmail.com>
2023-07-19 14:00:37 +0800

    Fix bug

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,8 +1,7 @@
+import { getToken } from "next-auth/jwt";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
- 
+
 const f = createUploadthing();
- 
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
  
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
@@ -11,7 +10,7 @@ export const ourFileRouter = {
     // Set permissions and file types for this FileRoute
     .middleware(async (req) => {
       // This code runs on your server before upload
-      const user = await auth(req);
+      const user = await getToken({req})
  
       // If you throw, the user will not be able to upload
       if (!user) throw new Error("Unauthorized");
